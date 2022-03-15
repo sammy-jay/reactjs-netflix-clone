ee0f5284a55adc96f0025edf5f94cc1c48641625
sammy-jay <sammyjayden27@gmail.com>
2022-03-15 02:12:58 +0100

    1 file modified, 1 added

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  apiKey: 'AIzaSyBJOGp7ByU-DrZilIoca0k82SMWs7sDc3I',
+  authDomain: 'reactjs-netflix-clone-46ea3.firebaseapp.com',
+  projectId: 'reactjs-netflix-clone-46ea3',
+  storageBucket: 'reactjs-netflix-clone-46ea3.appspot.com',
+  messagingSenderId: '593926729216',
+  appId: '1:593926729216:web:da48daecacafe82b05b2e5',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
