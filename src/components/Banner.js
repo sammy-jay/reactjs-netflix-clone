370df5cd4c6262e5cffaba16d5604dbc99f7f2c5
sammy-jay <sammyjayden27@gmail.com>
2022-03-15 03:49:09 +0100

    2 files modified, 3 added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+import requests from "../requests";
+import "./Banner.css";
 
-import './Banner.css';
-const Banner = () => {
-  return <div></div>;
-};
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      // request.data.results;
+      const random = Math.floor(Math.random() * request.data.results.length);
+      setMovie(request.data.results[random]);
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundPositon: "center",
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${
+          movie && movie.backdrop_path
+        }')`,
+      }}
+    >
+      <div className="banner__content">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">{`${
+          movie.overview && movie.overview.slice(0, 150)
+        } ...`}</h1>
+      </div>
+      <div className="banner__fadeBottom"></div>
+    </header>
+  );
+}
+
 export default Banner;
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import requests from '../requests';
 import Nav from './Nav';
 import Banner from './Banner';
+import Row from './Row';
 import './HomeScreen.css';
 
 const HomeScreen = () => {
@@ -12,6 +14,18 @@ const HomeScreen = () => {
       {/* Banner */}
       <Banner />
       {/* Row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
